b0073f5c03371aa3860ab244ecd06cb0ead82652
Ahmed Elfakharany <a.elfakharany@activevideo.com>
2021-05-16 15:51:20 +0200

    Fixing Redis service in CI

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-// const redis = require("redis");
-// const redisClient = redis.createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT });
-// redisClient.on('error', err => {
-//     console.log('Error ' + err);
-//     process.exit(1)
-// });
-console.log("Testing")
+const redis = require("redis");
+redis.createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT });
